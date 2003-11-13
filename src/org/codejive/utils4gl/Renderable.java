501ab55d6215f3f15fe58c2dcd46028538d7c6d8
tako <devnull@localhost>
2003-11-13 01:31:57 +0100

    [svn] Firsdt checkin. Moved it from the world3d package.

---
+++ src/org/codejive/utils4gl/Renderable.java
@@ -0,0 +1,15 @@
+/*
+ * Created on Jul 29, 2003
+ */
+package org.codejive.utils4gl;
+
+import org.codejive.utils4gl.*;
+
+/**
+ * @author tako
+ */
+public interface Renderable {
+	public boolean readyForRendering();
+	public void initRendering(RenderContext _context);
+	public void render(RenderContext _context);
+}
