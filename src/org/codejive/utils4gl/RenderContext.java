84fc26e197d7f2e349ae8b2cec2e7c510468ca66
tako <devnull@localhost>
2003-11-15 01:56:08 +0100

    [svn] Removed unnecessary import. Removed testing/debug code I had left in by mistake. Added setTexture().

--- src/org/codejive/utils4gl/RenderContext.java
+++ src/org/codejive/utils4gl/RenderContext.java
@@ -5,8 +5,6 @@ package org.codejive.utils4gl;
 
 import java.io.IOException;
 
-import org.codejive.utils4gl.*;
-
 import net.java.games.jogl.GL;
 import net.java.games.jogl.GLU;
 import net.java.games.jogl.util.GLUT;
@@ -26,24 +24,9 @@ public class RenderContext {
 		m_gl = _gl;
 		m_glu = _glu;
 
-		m_textures = new int[1];
-		_gl.glGenTextures(1, m_textures);
-		Texture texture = null;
-		try {
-			texture = TextureReader.readTexture("games/batoru/textures/grass_03.jpg");
-		} catch (IOException e) {
-			System.err.println("Could not read texture because " + e.getMessage());
-		}
-		_gl.glBindTexture(GL.GL_TEXTURE_2D, m_textures[0]);
-		if (texture != null) {
-			makeRGBTexture(texture, GL.GL_TEXTURE_2D, true);
-		}
-//		_gl.glTexParameteri(GL.GL_TEXTURE_2D,GL.GL_TEXTURE_MIN_FILTER,GL.GL_NEAREST);
-//		_gl.glTexParameteri(GL.GL_TEXTURE_2D,GL.GL_TEXTURE_MAG_FILTER,GL.GL_NEAREST);
-//		_gl.glTexParameteri(GL.GL_TEXTURE_2D,GL.GL_TEXTURE_MIN_FILTER,GL.GL_LINEAR);
-//		_gl.glTexParameteri(GL.GL_TEXTURE_2D,GL.GL_TEXTURE_MAG_FILTER,GL.GL_LINEAR);
-		_gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MIN_FILTER, GL.GL_LINEAR_MIPMAP_NEAREST);
-		_gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAG_FILTER, GL.GL_LINEAR);
+		// Just an arbitrary number for now until I decide how to handle this
+		m_textures = new int[10];
+		m_gl.glGenTextures(10, m_textures);
 	}
 	
 	public GL getGl() {
@@ -58,19 +41,28 @@ public class RenderContext {
 		return m_glut;
 	}
 	
+	public void setTexture(int _nTextureIndex, String _sFileName) {
+		Texture texture = null;
+		try {
+			texture = TextureReader.readTexture(_sFileName);
+		} catch (IOException e) {
+			System.err.println("Could not read texture because " + e.getMessage());
+		}
+		m_gl.glBindTexture(GL.GL_TEXTURE_2D, m_textures[_nTextureIndex]);
+		if (texture != null) {
+			makeRGBTexture(texture, GL.GL_TEXTURE_2D, true);
+		}
+	}
+
 	public int getTextureHandle(int _nTextureId) {
 		return m_textures[_nTextureId];
 	}
     
-	private void makeRGBTexture(Texture img, int target, boolean mipmapped)
-	{
-	  if (mipmapped)
-	  {
-		m_glu.gluBuild2DMipmaps(target, GL.GL_RGB8, img.getWidth(), img.getHeight(), GL.GL_RGB, GL.GL_UNSIGNED_BYTE, img.getPixels());
-	  }
-	  else
-	  {
-		m_gl.glTexImage2D(target, 0, GL.GL_RGB, img.getWidth(), img.getHeight(), 0, GL.GL_RGB, GL.GL_UNSIGNED_BYTE, img.getPixels());
-	  }
+	private void makeRGBTexture(Texture img, int target, boolean mipmapped) {
+		if (mipmapped) {
+			m_glu.gluBuild2DMipmaps(target, GL.GL_RGB8, img.getWidth(), img.getHeight(), GL.GL_RGB, GL.GL_UNSIGNED_BYTE, img.getPixels());
+		} else {
+			m_gl.glTexImage2D(target, 0, GL.GL_RGB, img.getWidth(), img.getHeight(), 0, GL.GL_RGB, GL.GL_UNSIGNED_BYTE, img.getPixels());
+		}
 	}
 }
